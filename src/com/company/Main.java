8b836d13786cf7e078607f7840fb82772f094a7e
Tales Takemiya <tales.takemiya@gmail.com>
2015-06-24 07:22:20 -0500

    Separating weekdays from weekends

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -26,6 +26,15 @@ import java.util.List;
  * Sat Mar 28 2015		0	0
  * Sun Mar 29 2015		1	0
  * Mon Mar 30 2015		7	1
+ *s
+ * And a run resume:
+ * ============================
+ *  total checkouts: 464
+ *  total denied: 671
+ *  total weekend checkouts: 54
+ *  total weekend denies: 0
+ *  total days on log: 43
+ * ============================
  */
 public class Main {
 
@@ -43,17 +52,22 @@ public class Main {
 		int deniedCount = 0;
 		int checkoutCountTotal = 0;
 		int deniedCountTotal = 0;
+		int weekendCheckOuts = 0;
+		int weekendDenies = 0;
 		boolean firstLoop = true;
+
 		try (Writer writer = new BufferedWriter(new OutputStreamWriter(
 				new FileOutputStream("output.csv"), "utf-8"))) {
 
 			writer.write("Date\tCheckout count\tDenied count\n");
+			String time = "";
 			// here we loop through all the lines in the file
 			for (String line : allLines) {
 				line = line.trim();
 				// lets get the date
+
 				if (line.contains("Time:")) {
-					String time = line.split("Time:")[1].trim();
+					time = line.split("Time:")[1].trim();
 					String[] datePieces = time.split(" ");
 					String weekDay = datePieces[0];
 
@@ -74,13 +88,25 @@ public class Main {
 				// if it is from the license we want we start counting the features checked out and denied
 				if (line.contains("parteklm") && line.contains("base")) {
 					firstLoop = false;
-					if (line.contains("OUT:")) {
-						checkoutCount++;
-						checkoutCountTotal++;
-					}
-					if (line.contains("DENIED:")) {
-						deniedCount++;
-						deniedCountTotal++;
+					// here we separate weekends from weekdays
+					if (time.startsWith("Sat") || time.startsWith("Sun")) {
+						if (line.contains("OUT:")) {
+							checkoutCount++;
+							weekendCheckOuts++;
+						}
+						if (line.contains("DENIED:")) {
+							deniedCount++;
+							weekendDenies++;
+						}
+					} else {
+						if (line.contains("OUT:")) {
+							checkoutCount++;
+							checkoutCountTotal++;
+						}
+						if (line.contains("DENIED:")) {
+							deniedCount++;
+							deniedCountTotal++;
+						}
 					}
 				}
 			}
@@ -88,12 +114,12 @@ public class Main {
 			writer.write("\t" + checkoutCount + "\t" + deniedCount + "\n");
 		}
 
-		printResume(daysOnLog, checkoutCountTotal, deniedCountTotal);
+		printResume(daysOnLog, checkoutCountTotal, deniedCountTotal, weekendCheckOuts, weekendDenies);
 	}
 
 	private static List<String> getAllLinesFromFile(String arg) {
 		File logFile = new File(arg);
-		List<String> allLines = new ArrayList<String>();
+		List<String> allLines = new ArrayList<>();
 		if (logFile.canRead()) {
 			try {
 				allLines = Files.readAllLines(logFile.toPath(), Charset.defaultCharset());
@@ -107,11 +133,13 @@ public class Main {
 		return allLines;
 	}
 
-	private static void printResume(int daysOnLog, int checkoutCountTotal, int deniedCountTotal) {
-		System.out.println("=======================");
+	private static void printResume(int daysOnLog, int checkoutCountTotal, int deniedCountTotal, int weekendCheckOuts, int weekendDenies) {
+		System.out.println("============================");
 		System.out.println(" total checkouts: " + checkoutCountTotal);
 		System.out.println(" total denied: " + deniedCountTotal);
+		System.out.println(" total weekend checkouts: " + weekendCheckOuts);
+		System.out.println(" total weekend denies: " + weekendDenies);
 		System.out.println(" total days on log: " + daysOnLog);
-		System.out.println("=======================");
+		System.out.println("============================");
 	}
 }
